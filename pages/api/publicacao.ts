1339ee73e38c3c765d02a469332e3877687df340
aaraujolua <f.araujoluana@gmail.com>
2023-08-28 16:16:54 -0300

    endpoint publicacao

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -1,26 +1,53 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiResponse } from "next";
 import type { respostaPadraoMsg } from "@/types/RespostaPadraoMsg";
 import nc from "next-connect";
 import { upload, uploadImagemCosmic } from "@/services/uploadImagemCosmic";
 import { conectarMongoDb } from "@/middlewares/conectarMongoDB";
 import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { publicacaoModel } from "@/models/PublicacaoModel";
+import { UsuarioModel } from "@/models/UsuarioModel";
 
 const handler = nc()
     .use(upload.single('file'))
-    .post(async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
-        const {descricao, file} = req?.body;
+    .post(async (req : any, res : NextApiResponse<respostaPadraoMsg>) => {
 
-        if(!descricao || descricao.length < 2) {
-            return res.status(400).json({error: 'Descrição inválida'});
-        };
+        try {
+            const {userId} = req.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({error: 'Usuário não encontrado'});
+            };
 
-        if(!file) {
-        return res.status(400).json({error: 'O arquivo selecionado não é suportado'})
-    };
+            if(!req || !req.body) {
+                return res.status(400).json({error: 'Parâmetros de entrada não informados'});
+            };
 
-        return res.status(400).json({error: 'Publicação carregada com sucesso'})
+            const {descricao} = req?.body;
+            if(!descricao || descricao.length < 2) {
+                return res.status(400).json({error: 'Descrição inválida'});
+            };
+        
+            if(!req.file) {
+                return res.status(400).json({error: 'O arquivo selecionado não é suportado'})
+            };
+
+            const image = await uploadImagemCosmic(req);
+            const publicacao = {
+                idUsuario : usuario.id,
+                descricao,
+                foto : image.media.url,
+                data : new Date()
+            };
+
+        await publicacaoModel.create(publicacao);
+        return res.status(200).json({msg: 'Publicação carregada com sucesso'})
+
+        }catch(e) {
+            console.log(e);
+            return res.status(400).json({error : 'Erro ao enviar publicação'});
+        }
     });
-
+        
 export const config = {
     api : {
         bodyParser : false
