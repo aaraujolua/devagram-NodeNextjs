a05842748b9ee8a5ceca00ec16464322fc21f2a5
aaraujolua <f.araujoluana@gmail.com>
2023-10-24 16:51:36 -0300

    comentarios.ts

---
+++ pages/api/comentarios.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { respostaPadraoMsg } from "@/types/RespostaPadraoMsg";
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { conectarMongoDb } from "@/middlewares/conectarMongoDB";
+import { usuarioModel } from "@/models/UsuarioModel";
+import { publicacaoModel } from "@/models/PublicacaoModel";
+import { politicaCORS } from '@/middlewares/politicaCORS';
+
+const comentarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
+    try{
+        if(req.method === 'PUT'){
+            const {userId, id} = req.query;
+            const usuarioLogado = await usuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({error : 'Usuário não encontrado'});
+            }
+            
+            const publicacao =  await publicacaoModel.findById(id);
+            if(!publicacao){
+                return res.status(400).json({error : 'Publiço não encontrada'});
+            }
+
+            if(!req.body || !req.body.comentario
+                || req.body.comentario.length < 2){
+                return res.status(400).json({error : 'Comentário não é válido'});
+            }
+
+            const comentario = {
+                usuarioId : usuarioLogado._id,
+                nome : usuarioLogado.nome,
+                comentario : req.body.comentario
+            }
+
+            publicacao.comentarios.push(comentario);
+            await publicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
+            return res.status(200).json({msg : 'Comentario adicionado com sucesso'});
+        }
+        
+        return res.status(405).json({error : 'Método informado não é válido'});
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({error : 'Erro ao adicionar comentário'});
+    }
+}
+
+export default politicaCORS(validarTokenJWT(conectarMongoDb(comentarioEndpoint)));
--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,6 +4,7 @@ import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 import { conectarMongoDb } from "@/middlewares/conectarMongoDB";
 import { usuarioModel } from "@/models/UsuarioModel";
 import { publicacaoModel } from "@/models/PublicacaoModel";
+import { seguidorModel } from "@/models/SeguidorModel";
 import { politicaCORS } from '@/middlewares/politicaCORS';
 
 const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg | any>) => {
@@ -18,13 +19,43 @@ const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<resposta
                     .find({idUsuario : usuario._id})
                     .sort({data : -1});
                     return res.status(200).json(publicacao);
+            } else {
+                const {userId} = req.query;
+                const usuarioLogado = await usuarioModel.findById(userId);
+                if(!usuarioLogado){
+                    return res.status(400).json({error : 'Usuário não encontrado'});
+                }
+                const seguidores = await seguidorModel.find({usuarioId : usuarioLogado._id});
+                const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId);
+
+                const publicacoes = await publicacaoModel.find({
+                    $or : [
+                        {idUsuario : usuarioLogado._id},
+                        {idUsuario : seguidoresIds}
+                    ]
+                })
+                .sort({data : -1});
+
+                const result = [];
+                for (const publicacao of publicacoes) {
+                   const usuarioDaPublicacao = await usuarioModel.findById(publicacao.idUsuario);
+                   if(usuarioDaPublicacao){
+                        const final = {...publicacao._doc, usuario : {
+                            nome : usuarioDaPublicacao.nome,
+                            avatar : usuarioDaPublicacao.avatar
+                        }};
+                        result.push(final);  
+                   }
+                }
+
+                return res.status(200).json(result);
             }
-            return res.status(200).json({error: 'Método informado não é válido'});
         }
-    } catch(e) {
+            return res.status(405).json({error : 'Método informado não é válido'});
+    }catch(e){
         console.log(e);
-        return res.status(400).json({error: 'Não foi possível obter o feed'});
     }
+    return res.status(400).json({error : 'Não foi possível obter o feed'});
 }
 
 export default politicaCORS(validarTokenJWT(conectarMongoDb(feedEndpoint)));
