15cc1a7c11bdb94a2c70a066c30c7d669735a299
aaraujolua <f.araujoluana@gmail.com>
2023-10-30 12:56:10 -0300

    CREATE comentario.ts

--- pages/api/comentario.ts
+++ pages/api/comentario.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { respostaPadraoMsg } from "@/types/RespostaPadraoMsg";
-import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 import { conectarMongoDb } from "@/middlewares/conectarMongoDB";
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 import { usuarioModel } from "@/models/UsuarioModel";
 import { publicacaoModel } from "@/models/PublicacaoModel";
 import { politicaCORS } from '@/middlewares/politicaCORS';
@@ -17,7 +17,7 @@ const comentarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<re
             
             const publicacao =  await publicacaoModel.findById(id);
             if(!publicacao){
-                return res.status(400).json({error : 'Publiço não encontrada'});
+                return res.status(400).json({error : 'Publicação não encontrada'});
             }
 
             if(!req.body || !req.body.comentario
@@ -33,7 +33,7 @@ const comentarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<re
 
             publicacao.comentarios.push(comentario);
             await publicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
-            return res.status(200).json({msg : 'Comentario adicionado com sucesso'});
+            return res.status(200).json({msg : 'Comentário adicionado com sucesso'});
         }
         
         return res.status(405).json({error : 'Método informado não é válido'});
